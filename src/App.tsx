4b1ceebd1ddc80c43ac725deb0f44375aeb46efa
huanhoangg <huanhoang0511@gmail.com>
2024-03-25 16:35:34 +0100

    finish the table in its simplest form

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,92 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Character } from "./Character";
+import { useQuery, gql } from "@apollo/client";
+import { Table } from "antd";
+
+const ALL_CHARACTERS = gql`
+  query {
+    allPeople {
+      people {
+        name
+        height
+        mass
+        species {
+          name
+        }
+        gender
+        eyeColor
+        homeworld {
+          name
+        }
+      }
+    }
+  }
+`;
 
 function App() {
+  const { loading, error, data } = useQuery(ALL_CHARACTERS);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Whoops... Something is wrong oh no baby!</p>;
+
+  const renderValue = (value: any) => {
+    return value !== null && value !== undefined && value !== "n/a"
+      ? value
+      : "-";
+  };
+
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+      render: (text: string) => renderValue(text),
+    },
+    {
+      title: "Species",
+      dataIndex: "species",
+      key: "species",
+      render: (species: any) => renderValue(species?.name),
+    },
+    {
+      title: "Gender",
+      dataIndex: "gender",
+      key: "gender",
+      render: (text: string) => renderValue(text),
+    },
+    {
+      title: "Height (cm)",
+      dataIndex: "height",
+      key: "height",
+      render: (text: number) => renderValue(text),
+    },
+    {
+      title: "Weight (kg)",
+      dataIndex: "mass",
+      key: "mass",
+      render: (text: number) => renderValue(text),
+    },
+    {
+      title: "Eye color",
+      dataIndex: "eyeColor",
+      key: "eyeColor",
+      render: (text: string) => renderValue(text),
+    },
+    {
+      title: "Home planet",
+      dataIndex: "homeworld",
+      key: "homeworld",
+      render: (homeworld: any) => renderValue(homeworld?.name),
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Star Wars Characters</h1>
+      <Table
+        dataSource={data.allPeople.people}
+        columns={columns}
+        pagination={false}
+      />
     </div>
   );
 }
